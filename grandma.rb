3af5496d240e38c9588f3a62134d1dd6d45ab421
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-29 18:27:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase
-    "NO, NOT SINCE 1938!"
-  else
+  elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 end
 
